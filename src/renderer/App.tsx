57cabb89f75e5fd58b51174bdb12fcc3a112a56c
Ethan Zeng <ethan.z.zeng@gmail.com>
2025-10-25 11:34:57 -0400

    removed placeholders for another placeholder (lol)

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -10,32 +10,10 @@ function Hello() {
       <div className="Hello">
         <img width="200" alt="icon" src={testImage} />
       </div>
-      <h1>electron-react-boilerplate</h1>
       <div className="Hello">
-        <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
-          href="https://github.com/sponsors/electron-react-boilerplate"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="folded hands">
-              🙏
-            </span>
-            Donate
-          </button>
-        </a>
+        <button type="button">
+          Listen
+        </button>
       </div>
     </div>
   );
